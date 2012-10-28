83146915690d84536a2424de274f7158b2d29cf1
AleksandrBo <OleksandrBoris@gmail.com>
2012-10-28 08:39:15 +0200

    email options

--- ExceptionReporter.pas
+++ ExceptionReporter.pas
@@ -2,14 +2,15 @@ unit ExceptionReporter;
 // по материалам http://www.rsdn.ru/article/Delphi/DelphiJCL.xml
 interface
 
-uses SysUtils, Classes, JclDebug, JclHookExcept, dialogs;
+uses SysUtils, Classes, JclDebug, JclHookExcept, dialogs, IdSMTP, IdMessage;
 
 Type
   THandleExceptions = (heAll, heNotHandled);
 
 const
   HandleException = heNotHandled;
-
+  SendOnEmail = True;
+  {$I email.options.inc}
 type
   TExceptionHandler = class
   public
@@ -22,7 +23,32 @@ var
 
 implementation
 
-uses ComObj;
+procedure Send();
+var
+  SMTP: TIdSMTP;
+  IdMessage1: TIdMessage;
+begin
+  SMTP := TIdSMTP.Create(nil);
+  IdMessage1 := TIdMessage.Create(nil);
+  try
+    SMTP.Host := SMTP_HOST;
+    SMTP.Port := SMTP_PORT; //
+    SMTP.AuthenticationType := atLogin;
+    SMTP.Username := SMTP_USERNAME;
+    SMTP.Password := SMTP_PASSWORD;
+    //SMTP.
+    IdMessage1.Recipients.EMailAddresses:= TO_ADDRESS;
+    IdMessage1.From.Text := FROM_ADDRESS;
+    IdMessage1.Subject := 'Exception Report';
+    IdMessage1.Body.Text := 'hello';
+    SMTP.Connect;
+    SMTP.Send(IdMessage1);
+    SMTP.Disconnect; 
+  finally
+    FreeAndNil(smtp);
+    FreeAndNil(IdMessage1);
+  end;
+end;
 
 procedure AnyExceptionNotify(
   ExceptObj: TObject; ExceptAddr: Pointer; OSException: Boolean);
